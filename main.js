f4529531a41e5a8fc3f42323afad274bf243fcc8
Jazmin <you@example.com>
2022-01-10 00:19:32 -0300

    last changes

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector(".menu-principal-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+/*
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });*/
